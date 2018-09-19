811bdcab723fbf3290eae34302771be610732fe3
Sydney Rubin <syd.l.rubin@gmail.com>
2018-09-19 14:32:10 -0700

    SR work on vertical slice 9.19

---
+++ vertical slice 532 copy/Assets/UIManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	public InputField intentionInput;
+
+
+	void RevealTextInput(){
+		intentionInput.ActivateInputField ();
+	}
+}
