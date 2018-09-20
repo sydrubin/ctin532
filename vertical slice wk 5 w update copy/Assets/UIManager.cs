e113581a4ab03b16acc78deae40abe21fdd11b83
Sydney Rubin <syd.l.rubin@gmail.com>
2018-09-20 12:30:05 -0700

    more playable version with placeholders where i think mechanic intro could go

---
+++ vertical slice wk 5 w update copy/Assets/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public GameObject myInputField;
+	
+    void ShowInputField(){
+        myInputField.SetActive(true);
+    }
+
+    void HideInputField()
+    {
+        myInputField.SetActive(false);
+    }
+}
